c1fa456b2f54984bad8e7496321dd34bb04b294b
Dillassh <dill.assh@gmail.com>
2022-10-04 23:31:12 +0300

    fixpages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/common/NavBar/NavBar.js";
-import Footer from "./components/common/Footer/Footer.js";
-import Home from "./views/Home/Home.js";
-import Prices from "./views/Prices/Prices.js";
-import AboutUs from "./views/AboutUs/AboutUs.js";
-import Contact from "./views/Contact/Contact.js";
+import {
+	BrowserRouter as Router,
+	Routes as Switch,
+	Route,
+} from "react-router-dom";
+import NavBar from "./components/common/NavBar/NavBar";
+import Footer from "./components/common/Footer/Footer";
+import Home from "./views/Home/Home";
+import Prices from "./views/Prices/Prices";
+import AboutUs from "./views/AboutUs/AboutUs";
+import Contact from "./views/Contact/Contact";
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
 				<NavBar className='fixed-top' />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/prices' element={<Prices />} />
-					<Route path='/aboutus' element={<AboutUs />} />
+				<Switch>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/prices' element={<Prices />} />
+					<Route exact path='/aboutus' element={<AboutUs />} />
 					<Route path='/contact' element={<Contact />} />
-				</Routes>
+				</Switch>
 				<Footer />
 			</Router>
 		</div>
