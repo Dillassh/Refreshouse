7144a6f7503583a3d914c8afb4a30d7c92c8c1d8
Dillassh <dill.assh@gmail.com>
2022-10-04 22:34:23 +0300

    correction

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../../components/common/Footer/Footer";
-import imagine from "./slide1.jpg";
+
 import "./Home.css";
 import ContainerHome from "../../components/home/ContainerHome";
 import CarouselFade from "../../components/home/carousel/CarouselFade";
