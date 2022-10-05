e83b24c9a605fa8ff58d8a998ffdb70f1c69ce62
Dillassh <dill.assh@gmail.com>
2022-10-05 12:25:52 +0300

    changes

--- src/components/home/carousel/CarouselFade.js
+++ src/components/home/carousel/CarouselFade.js
@@ -1,52 +1,68 @@
 import Carousel from "react-bootstrap/Carousel";
 import "./CarouselFade.css";
 import produse from "./produse.jpg";
-import kit from "./kit-curatenie.jpg";
+import kit from "./tools.png";
 import vacuum from "./vacuum.png";
 
 function CarouselFade() {
 	return (
 		<Carousel fade>
 			<Carousel.Item>
-				<img
-					className='d-block carousel-content carousel-one'
-					src={produse}
-					alt='First slide'
-				/>
-				<Carousel.Caption>
-					<div className='first-slide slide'>
-						<h3>Produse profesionale create pentru nevoile tale.</h3>
-						<button>DESCOPERA</button>
+				<div className='slide1'>
+					<div>
+						<img
+							className='d-block carousel-content carousel-one'
+							src={produse}
+							alt='First slide'
+						/>
 					</div>
-				</Carousel.Caption>
+					<div>
+						<Carousel.Caption>
+							<div className='first-slide slide'>
+								<h3>Produse profesionale create pentru nevoile tale.</h3>
+								<button>DESCOPERA</button>
+							</div>
+						</Carousel.Caption>
+					</div>
+				</div>
 			</Carousel.Item>
 			<Carousel.Item>
-				<img
-					className='d-block carousel-content'
-					src={kit}
-					alt='Second slide'
-				/>
-
-				<Carousel.Caption>
-					<div className='second-slide slide'>
-						<h3>Servicii profesionale de curatare.</h3>
-						<button>DESCOPERA</button>
+				<div className='slide1'>
+					<div>
+						<img
+							className='d-block carousel-content'
+							src={kit}
+							alt='Second slide'
+						/>
 					</div>
-				</Carousel.Caption>
+					<div>
+						<Carousel.Caption>
+							<div className='second-slide slide'>
+								<h3>Servicii profesionale de curatare.</h3>
+								<button>DESCOPERA</button>
+							</div>
+						</Carousel.Caption>
+					</div>
+				</div>
 			</Carousel.Item>
 			<Carousel.Item>
-				<img
-					className='d-block carousel-content'
-					src={vacuum}
-					alt='Third slide'
-				/>
-
-				<Carousel.Caption>
-					<div className='third-slide slide'>
-						<h3>Echipamente profesionale!</h3>
-						<button>CONTACTATI-NE</button>
+				<div className='slide1'>
+					<div>
+						<img
+							className='d-block carousel-content'
+							src={vacuum}
+							alt='Third slide'
+						/>
 					</div>
-				</Carousel.Caption>
+					<div>
+						<Carousel.Caption>
+							<div className='third-slide slide'>
+								<h3>Echipamente profesionale!</h3>
+								<button>CONTACTATI-NE</button>
+							</div>
+						</Carousel.Caption>
+					</div>
+				</div>
 			</Carousel.Item>
 		</Carousel>
 	);
